321f9166170f48880bc4122b8ca886986f78f8f0
octopi <octopie@gmail.com>
2012-01-29 04:34:24 -0500

    fb start

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source :rubygems
 gem 'sinatra', '1.1.0'
 gem 'thin', '1.2.7'
+gem 'mongo', '1.5.2'
+gem 'bson_ext', '1.5.2'
--- web.rb
+++ web.rb
@@ -4,9 +4,17 @@ require 'uri'
 require 'mongo'
 
 get '/' do
-	"Hello world"
-  	uri  = URI.parse(ENV['MONGOLAB_URI'])
-  	conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
-  	db   = conn.db(uri.path.gsub(/^\//, ''))
-  	db.collection_names.each { |name| puts name }
+	erb :index
+  	
 end
+
+#uri  = URI.parse(ENV['MONGOLAB_URI'])
+  	#conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
+  	#db   = conn.db(uri.path.gsub(/^\//, ''))
+  	#users_coll = db.collection("users")
+  	##test = {"name" => "David", "id" => 123123123}
+  	#users_coll.insert(test)
+
+  	#users_coll.find().each { |row|
+  #		puts row.inspect
+  #	}
