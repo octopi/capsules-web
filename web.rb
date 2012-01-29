000c2027691eaf8f8ea27a2414cc4d3d02af1731
octopi <octopie@gmail.com>
2012-01-28 21:53:29 -0500

    testing mongo

--- web.rb
+++ web.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
+require 'rubygems'
+require 'uri'
+require 'mongo'
 
 get '/' do
-  "Hello world"
+	"Hello world"
+  	uri  = URI.parse(ENV['MONGOLAB_URI'])
+  	conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
+  	db   = conn.db(uri.path.gsub(/^\//, ''))
+  	db.collection_names.each { |name| puts name }
 end
