7ff1f69819d088d425a885fb302f3ebd572c983d
octopi <octopie@gmail.com>
2012-01-29 05:33:49 -0500

    post

--- web.rb
+++ web.rb
@@ -7,15 +7,16 @@ get '/' do
 	erb :index
 end
 
-get '/login/:user_id' do
+post '/login' do
 	uri  = URI.parse(ENV['MONGOLAB_URI'])
   	conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
   	db   = conn.db(uri.path.gsub(/^\//, ''))
   	users_coll = db.collection("users")
-  	puts params[:user_id]
-  	user = users_coll.find("id" => params[:user_id]).each { |row|
-  		puts row.inspect
-  	}
+  	if users_coll.count(:query => {"id" => params[:user_id]}) == 0
+  		new_user = {"id" => params[:user_id]}
+  		users_coll.insert(new_user)
+  		"inserted new user"
+  	end
 end
 
 #uri  = URI.parse(ENV['MONGOLAB_URI'])
