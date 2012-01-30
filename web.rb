d31ece0a7a165e383b2b87ea3d03e895274f819a
octopi <octopie@gmail.com>
2012-01-30 02:37:50 -0500

    new capsule

--- web.rb
+++ web.rb
@@ -3,6 +3,8 @@ require 'rubygems'
 require 'uri'
 require 'mongo'
 
+enable :sessions
+
 get '/' do
 	erb :index
 end
@@ -12,11 +14,28 @@ post '/login' do
   	conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
   	db   = conn.db(uri.path.gsub(/^\//, ''))
   	users_coll = db.collection("users")
-  	if users_coll.count(:query => {"id" => params[:user_id]}) == 0
-  		new_user = {"id" => params[:user_id]}
+  	if users_coll.count(:query => {"fbid" => params[:user_id]}) == 0
+  		new_user = {"fbid" => params[:user_id]}
   		users_coll.insert(new_user)
   		"inserted new user"
   	end
+  	user = users_coll.find({"fbid" => params[:user_id]}).first
+  	"b "+user['_id'].to_s
+  	sessions['ocid'] = user['_id'].to_s
+  	redirect '/me'
+end
+
+get '/me' do
+	erb :me
+end
+
+post '/new_capsule' do
+	uri  = URI.parse(ENV['MONGOLAB_URI'])
+  	conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
+  	db   = conn.db(uri.path.gsub(/^\//, ''))
+  	capsules = db.collection("capsules")
+  	new_capsule = {"strKey" => params[:strKey]}
+  	capsules.insert(new_capsule);
 end
 
 #uri  = URI.parse(ENV['MONGOLAB_URI'])
